d4826e9a60663dd6a4857cc919fac1a1131d4ed3
sara <sarangan12@gmail.com>
2019-03-29 14:06:37 +0800

    Update cron_process.py

--- codebuild/cron_process.py
+++ codebuild/cron_process.py
@@ -3,12 +3,12 @@ import json
 from botocore.vendored import requests
 from datetime import datetime
 
-account_id = "4802324438001"
-client_id = "8fde413c-6b87-4813-af22-a4908eb3cdc6"
-client_secret = "XnnNE3mJ4NEuy_WOvbA2yKGylYkV1t7MhLx1THdDJXofRx-h8CB-AamYCn6J_unxyJLOyk8gfqsc6-iS8OVF_g"
-access_token_url = "https://oauth.brightcove.com/v4/access_token"
-profiles_base_url = "https://cms.api.brightcove.com/v1/accounts/{account_id}"
-dynamodb_table = "dev_bc_videos"
+account_id = "XXXX"
+client_id = "XXXXX"
+client_secret = "XXXXX"
+access_token_url = "XXXX"
+profiles_base_url = "XXXXX"
+dynamodb_table = "XXXX"
 
 dynamo_db = boto3.client('dynamodb')
 
